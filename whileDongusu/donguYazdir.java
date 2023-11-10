6f54f8b16ff06d09ec2f0e0957322c7c16728247
Elif Vural <elifvural019@gmail.com>
2023-11-10 03:06:51 +0300

    hijjj

---
+++ whileDongusu/donguYazdir.java
@@ -0,0 +1,32 @@
+package whileDongusu;
+
+public class donguYazdir {
+
+    //while dongusunde kosul ilk cumlede yer alir. Ayni zamanda donguden cikis kosuludur.
+
+   /*
+
+    while(kosul){
+        s1;
+        s2;
+    }
+
+    */
+    public static void main(String[] args) {
+        
+
+        int j=1;
+        while(j<=10){
+            System.out.println("j: "+j);
+           j++;
+        }
+
+        //sonsuz dongu
+        int i=0;
+        while(1==1){
+            i++;
+            System.out.println(i);
+        }
+    }
+    
+}
