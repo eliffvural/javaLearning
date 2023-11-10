5f06b7670acea3f7fe62facbca4d7a2171eb863e
Elif Vural <elifvural019@gmail.com>
2023-11-11 01:58:03 +0300

    hijjj

---
+++ forDongusu/forDongu.java
@@ -0,0 +1,25 @@
+package forDongusu;
+
+public class forDongu {
+    public static void main(String[] args) {
+        
+        //for dongusunde dongu tekrarini saglayan bir sayac vardir.
+        //bu sayac sayesinde dongudeki tekrar sayisi sabitlenmistir.
+        //yani sayac sayesinde  dongunun ne kadar tekrar edilecegi en basindan bellidir.
+
+
+        int i;
+        for(i=1;i<=5;i++){
+            System.out.println(i+". kez isletildi.");
+        }
+
+        //1 ile 5 arasindaki sayac degerlerini yazdirmak uzere for dongusu yazalim:
+
+        int sayac;
+        for(sayac=1;sayac<=5;sayac++){
+            System.out.println("Sayac: "+sayac);
+        }
+
+    }
+    
+}
